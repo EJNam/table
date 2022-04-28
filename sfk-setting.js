a86162c45167ee99c49504ef740ab4f9ac145724
EJNam <36287495+EJNam@users.noreply.github.com>
2022-04-28 22:30:19 +0100

    Update sfk-setting.js

--- sfk-setting.js
+++ sfk-setting.js
@@ -1,9 +1,27 @@
-const setTranslateQuery = (lang) => {
-    const render = (res) => res[0].map(r => r[0]).join('');
-    Front.registerInlineQuery({
-        url: `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&dt=bd&q=`,
-        parseResult: res => [ render(JSON.parse(res.text)) ],
-    });
-};
-
-setTranslateQuery('ru');
+Front.registerInlineQuery({
+    url: function(q) {
+        return `http://dict.youdao.com/w/eng/${q}/#keyfrom=dict2.index`;
+    },
+    parseResult: function(res) {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(res.text, "text/html");
+        var collinsResult = doc.querySelector("#collinsResult");
+        var authTransToggle = doc.querySelector("#authTransToggle");
+        var examplesToggle = doc.querySelector("#examplesToggle");
+        if (collinsResult) {
+            collinsResult.querySelectorAll("div>span.collinsOrder").forEach(function(span) {
+                span.nextElementSibling.prepend(span);
+            });
+            collinsResult.querySelectorAll("div.examples").forEach(function(div) {
+                div.innerHTML = div.innerHTML.replace(/<p/gi, "<span").replace(/<\/p>/gi, "</span>");
+            });
+            var exp = collinsResult.innerHTML;
+            return exp;
+        } else if (authTransToggle) {
+            authTransToggle.querySelector("div.via.ar").remove();
+            return authTransToggle.innerHTML;
+        } else if (examplesToggle) {
+            return examplesToggle.innerHTML;
+        }
+    }
+});
